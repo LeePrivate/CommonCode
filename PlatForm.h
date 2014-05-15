e4020ea89c9a58db07e59ee84c0b5e361709d025
LEE <year520017@163.com>
2014-05-15 16:46:23 +0800

    增加跨平台宏定义 mac 平台的宏还不知道 暂时写了个MAC

---
+++ PlatForm.h
@@ -0,0 +1,33 @@
+/*!
+ * \file PlatForm.h
+ *
+ * \author lixiang
+ * \date 五月 2014
+ *
+ * 平台相关宏定义;
+ */
+
+#pragma once
+
+#define PLATFORM_WINDOWS 1;
+#define PLATFORM_LINUX 2;
+#define PLATFORM_MAC 3;
+
+
+// Win
+#if defined(WIN32) || defined(WIN64) || defined(_WINDOWS_)
+#undef  TARGET_PLATFORM
+#define TARGET_PLATFORM			PLATFORM_WINDOWS
+#endif
+
+// Linux
+#if defined(LINUX)
+#undef  TARGET_PLATFORM
+#define TARGET_PLATFORM         PLATFORM_LINUX
+#endif
+
+// Mac
+#if defined(MAC)
+#undef  TARGET_PLATFORM
+#define TARGET_PLATFORM         PLATFORM_MAC
+#endif
