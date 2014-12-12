74200e63348ce4d58b9b4e53a9fa582dd7975718
lee <year520017@163.com>
2014-12-12 09:42:19 +0800

    DataSystem 配置文件读取系统完成;

--- DataSystem/ConfigLibrary.hpp
+++ DataSystem/ConfigLibrary.hpp
@@ -1,12 +1,25 @@
+/********************************************************************
+	created:	2014/12/11
+	created:	11:12:2014   9:43
+
+	author:		李翔
+	
+	purpose:	配置文件读取器,一些简单的两行的配置就用这个类读取存储(如:服务器IP配置);
+	配置文件写法:(注意是CSV文件);
+	name$string,	server
+	ip$string,			192.168.1.168
+	port$int,			8010 
+*********************************************************************/
+
+
 #pragma once
 
-#include "CSVReader.hpp"
+#include "CsvParser/CsvReader.h"
 
 #include "BasicData.h"
-#include "StringModifier.h"
+#include "StringUtil/StringUtil.h"
 
-class ConfigLibrary
-	: public CSVReader
+class ConfigLibrary : public CSVReader
 {
 public:
 
@@ -29,26 +42,18 @@ public:
 	}
 
 protected:
-
-	virtual void parseLine(
-		unsigned int aIndex, 
-		const std::vector<std::string> &aRow) override
+	virtual void ParseLine(unsigned int aIndex, const std::vector<std::string>& aRow) override
 	{
 		assert(aRow.size() == 2);
 		std::string head = aRow[0];
-		std::vector<std::string> splitedHead = StringModifier::split(
-			head,
-			"$");
+		std::vector<std::string> splitedHead = StringUtil::Split(head, "$");
 		assert(splitedHead.size() == 2);
 		std::string key = splitedHead[0];
 		std::string type = splitedHead[1];
 
-		std::string value = aRow[1];
+		std::string val = aRow[1];
 
-		_data.parseData(
-			key,
-			type,
-			value);
+		_data.ParseData(key, type, val);
 	}
 
 	BasicData _data;
